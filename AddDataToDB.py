9e2c287c47ddf708e43c82c02a9fd9fd05e6bdf8
Soumyadeep <soumyadeepmukherjee6527@gmail.com>
2023-01-12 20:13:41 +0530

    Face recognition and database setup

---
+++ AddDataToDB.py
@@ -0,0 +1,46 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://smart-attendance-system-c2a01-default-rtdb.firebaseio.com/"
+})
+
+ref=db.reference('Students')
+
+data={
+    "1":
+        {
+            "name":"Soumyadeep Mukherjee",
+            "major":"Information Technology",
+            "starting_year":2019,
+            "total_attendance":5,
+            "grade": "O",
+            "year":4,
+            "last_attendance_time":"2023-01-10 11:30:00"
+        },
+    "2":
+        {
+            "name":"Sundar Pichai",
+            "major":"Computer Science",
+            "starting_year":2020,
+            "total_attendance":7,
+            "grade": "O",
+            "year":3,
+            "last_attendance_time":"2023-01-10 11:30:00"
+        },
+    "3":
+        {
+            "name":"Elon Musk",
+            "major":"Physics",
+            "starting_year":2021,
+            "total_attendance":4,
+            "grade": "E",
+            "year":2,
+            "last_attendance_time":"2023-01-10 11:30:00"
+        }
+}
+
+for key,value in data.items():
+    ref.child(key).set(value)
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import os
 import cv2
+import pickle
+import cvzone
+import face_recognition
+import numpy as np
 
 cap=cv2.VideoCapture(0)
 cap.set(3,640)
@@ -15,11 +19,43 @@ for path in modePathList:
     imgModeList.append(cv2.imread(os.path.join(folderModePath,path)))
 # print(len(imgModeList))
 
+# Load the encoding file
+print("Loading Encode File...")
+file=open('EncodeFile.p','rb')
+encodeListKnownWithIds=pickle.load(file)
+file.close()
+encodeListKnown, studentIds = encodeListKnownWithIds
+print(studentIds)
+print("Encode File Loaded...")
+
 while True:
     success,img=cap.read()
 
+    imgS=cv2.resize(img,(0,0), None, 0.25, 0.25)   # Scaling down the image to 1/4th
+    imgS=cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
+
+    faceCurrFrame=face_recognition.face_locations(imgS)
+    encodeCurrFrame=face_recognition.face_encodings(imgS,faceCurrFrame)
+
     imgBackground[162:162+480,55:55+640]=img  # Overlaying the image upon the background template
     imgBackground[44:44+633, 808:808 + 414] = imgModeList[0]
+
+    for encodeFace,faceLoc in zip(encodeCurrFrame,faceCurrFrame):
+        matches=face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis=face_recognition.face_distance(encodeListKnown,encodeFace)   # Lower the face distance , better the match
+        # print("Matches:",matches)
+        # print("Face Distance:",faceDis)
+
+        matchIndex=np.argmin(faceDis)
+        # print("Match Index",matchIndex)
+
+        if matches[matchIndex]:
+            print("Known Face Detected!")
+            y1,x2,y2,x1=faceLoc
+            y1,x2,y2,x1=y1*4,x2*4,y2*4,x1*4    # Scaling up 4 times coz it was scaled down previously
+            bbox= 55+x1,162+y1,x2-x1,y2-y1     # Creating the bounding box
+            imgBackground=cvzone.cornerRect(imgBackground,bbox,rt=0)
+
 
     # cv2.imshow("Webcam",img)
     cv2.imshow("Face Attendance", imgBackground)
