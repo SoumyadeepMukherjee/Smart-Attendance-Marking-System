9e24f61f83aa26f53f574c01fc2a187cede35304
Soumyadeep <soumyadeepmukherjee6527@gmail.com>
2023-01-12 20:55:42 +0530

    Uploading imgs to DB

--- encodeGenerator.py
+++ encodeGenerator.py
@@ -2,6 +2,16 @@ import cv2
 import face_recognition
 import pickle
 import os
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    'databaseURL': "https://smart-attendance-system-c2a01-default-rtdb.firebaseio.com/",
+    'storageBucket': "smart-attendance-system-c2a01.appspot.com"
+})
 
 # Importing the student images
 folderPath = 'Images'
@@ -13,6 +23,12 @@ studentIds=[]
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath,path)))
     studentIds.append(os.path.splitext(path)[0])
+
+    fileName= f'{folderPath}/{path}'
+    bucket=storage.bucket()
+    blob=bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
 print(studentIds)
 
 def findEncodings(imagesList):
