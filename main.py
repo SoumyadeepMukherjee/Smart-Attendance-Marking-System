71a70ad272f3ce649a1160ac4b31c79e1874178a
Soumyadeep <soumyadeepmukherjee6527@gmail.com>
2023-01-15 13:24:16 +0530

    Updating attendance data

--- main.py
+++ main.py
@@ -93,35 +93,57 @@ while True:
             array = np.frombuffer(blob.download_as_string(),np.uint8)
             imgStudent=cv2.imdecode(array,cv2.COLOR_BGRA2BGR)
 
+            # Updating data of attendance
+            ref=db.reference(f'Students/{id}')
+            studentInfo['total_attendance']+=1
+            ref.child('total_attendance').set(studentInfo['total_attendance'])
+
+        # ModeType becomes 2 when no. of frames are between 10 and 20
+        if 10<counter<20:
+            modeType = 2
+
+        imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
+
+
+        # When no. of frames are less than 10
         # Dynamically updating attendance, mode and other info from the DB
-        cv2.putText(imgBackground,str(studentInfo['total_attendance']),(861,125),
-                    cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
+        if counter<=10:
+            cv2.putText(imgBackground,str(studentInfo['total_attendance']),(861,125),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),1)
 
-        cv2.putText(imgBackground, str(studentInfo['major']), (1006, 550),
-                    cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+            cv2.putText(imgBackground, str(studentInfo['major']), (1006, 550),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
 
-        cv2.putText(imgBackground, str(id), (1006, 493),
-                    cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+            cv2.putText(imgBackground, str(id), (1006, 493),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
 
-        cv2.putText(imgBackground, str(studentInfo['grade']), (910, 625),
-                    cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+            cv2.putText(imgBackground, str(studentInfo['grade']), (910, 625),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
 
-        cv2.putText(imgBackground, str(studentInfo['year']), (1025, 625),
-                    cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+            cv2.putText(imgBackground, str(studentInfo['year']), (1025, 625),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
 
-        cv2.putText(imgBackground, str(studentInfo['starting_year']), (1125, 625),
-                    cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
+            cv2.putText(imgBackground, str(studentInfo['starting_year']), (1125, 625),
+                        cv2.FONT_HERSHEY_COMPLEX, 0.6, (100, 100, 100), 1)
 
-        # Resizing and centering the name
-        (w,h), _ = cv2.getTextSize(studentInfo['name'],cv2.FONT_HERSHEY_COMPLEX, 1,1)
-        offset=(414-w)//2
-        cv2.putText(imgBackground, str(studentInfo['name']), (808+offset, 445),
-                    cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50), 1)
+            # Resizing and centering the name
+            (w,h), _ = cv2.getTextSize(studentInfo['name'],cv2.FONT_HERSHEY_COMPLEX, 1,1)
+            offset=(414-w)//2
+            cv2.putText(imgBackground, str(studentInfo['name']), (808+offset, 445),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (50, 50, 50), 1)
 
-        # Putting the image dynamically from DB
-        imgBackground[175:175+216,909:909+216] = imgStudent
+            # Putting the image dynamically from DB
+            # imgBackground[175:175+216,909:909+216] = imgStudent
 
         counter += 1
+
+        # Resetting the values & mode after attendance is marked
+        if counter>=20:
+            counter=0
+            modeType=0
+            studentInfo=[]
+            imgStudent=[]
+            imgBackground[44:44 + 633, 808:808 + 414] = imgModeList[modeType]
 
     # cv2.imshow("Webcam",img)
     cv2.imshow("Face Attendance", imgBackground)
